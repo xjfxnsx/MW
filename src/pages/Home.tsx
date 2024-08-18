b89fe6093e4caefa937a603cfc2a8e2002424d04
Dzhafar Nasirov <xjfxnsx@gmail.com>
2024-08-18 17:54:58 +0400

    style fix for mob. dev.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ const Home: React.FC = () => {
   return (
     <div>
       <h1 style={{margin: '10px 100px', color: 'black', cursor: 'default'}}>Movie</h1>
-      <h1 style={{margin: '-10px 100px', color: 'white', cursor: 'default'}}>Watch</h1>
+      <h1 style={{margin: '-10px 100px 10px 100px', color: 'white', cursor: 'default'}}>Watch</h1>
       <MovieList />
     </div>
   );
